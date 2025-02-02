1415c316f38a413037fb5a7e476fc3d755e2048b
chaksseu <rtrt505@korea.ac.kr>
2025-02-02 14:48:26 +0000

    update_0202

--- setting.sh
+++ setting.sh
@@ -13,9 +13,10 @@ conda activate mmg
 #conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia -y
 #pip install -U pip
 #pip install -r requirements.txt
+#pip install -U torchvision
 #chown -R 1001:1001 /workspace
 #conda install -c conda-forge mpi4py -y
 #conda install cuda-cudart cuda-version=12 -y
-###conda install -c conda-forge moviepy -y
+#conda install -c conda-forge moviepy -y
 chown -R 1001:1001 /workspace
 export HF_HOME=/workspace/huggingface_cache
